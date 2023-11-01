7f25d6830ccc3bbf83f13c555eb177adcfd28c09
Bruno Leal <lealbrunocalhau@gmail.com>
2023-11-01 18:43:59 -0300

    fix: deprecated setup-nodejs.sh

--- setup-nodejs.sh
+++ setup-nodejs.sh
@@ -4,10 +4,17 @@ NODE_VERSION=$(node -v);
 
 NODE_MAJOR=$(echo "$NODE_VERSION" | cut -d. -f1);
 
-if [ "v18" = "$NODE_MAJOR" ]; then
+if [ "v20" = "$NODE_MAJOR" ]; then
   echo "NodeJS detected!"
   echo "Version: $NODE_VERSION"
   echo "Skipping setup..."
 else
-  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt-get install -y nodejs
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl gnupg
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  NODE_MAJOR=20
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  sudo apt-get update
+  sudo apt-get install nodejs -y
 fi
